4b38d31e73228d0eb3a3dba36c34672e12fa5d55
Lalib <louis.alib19@gmail.com>
2020-04-23 17:06:53 +0200

    routing work

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,37 @@
 const express = require("express");
 const router = express.Router();
+const Sneaker = require("../models/Sneaker")
 
 router.get("/", (req, res) => {
   res.render("index");
 });
 
 router.get("/sneakers/:cat", (req, res) => {
-  res.render("products");
+  console.log(req.params);
+  const cat = req.params.id;
+  Sneaker.find(cat)
+    .populate("id_tags")
+    .then(dbResult => {
+      res.render("/products_manage.hbs", {
+        sneakers: dbResult,
+
+      });
+    })
+    .catch(dbErr => {
+      console.log(dbErr);
+    })
 });
 
 router.get("/one-product/:id", (req, res) => {
-  res.send("baz");
+  // Sneaker.find()
+  // .populate("id_tags")
+  // .then(dbResult => {
+  //   sneakers: dbResult,
+
+  // })
+  // .catch(dbErr => {
+  //   console.log(dbErr);
+  // })
 });
 
 router.get("/signup", (req, res) => {
